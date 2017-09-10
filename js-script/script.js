f277450109604359b0d777c2b40fd36e9129042a
Kevin <ruraegor@gmail.com>
2017-09-11 00:33:26 +0300

    gallery and description

--- js-script/script.js
+++ js-script/script.js
@@ -11,12 +11,20 @@ $(function () {
 
 			$(box).addClass("ttttttttt");
 
-			console.log(box);
 
-			if ($(box).hasClass("slider-art-wow")) {
+			//------- last-update
+			if ( $(box).hasClass("last-update__img") ) {
+
+				$(box).addClass("active");
+			}
+
+
+			//------- art-slider
+			if ( $(box).hasClass("slider-art-wow") ) {
 
 				setTimeout("startSl()", 2000);
 			}
+			
 		}
 	});
 
@@ -68,6 +76,11 @@ $(function () {
 	});
 
 
+	//==========  MAIN MENU SCROLL
+
+	
+
+
 	// $('#wrap').slimScroll({
 	// 	height: 'auto'
 	// });
@@ -90,8 +103,13 @@ $(function () {
 	// });
 
 
+
+	//==========  CHARACTERS-MODAL
+	//$("..js-characters-modal").fancybox();
+
+
 	//==========  FANCYBOX
-	//$(".js-characters-modal").fancybox();
+	$(".fanc-gall-img").fancybox();
 
 });
 
@@ -140,9 +158,13 @@ $(document).keydown(function (evt) {
 
 
 var ms_img_src = [0];
+var ms_img_src_min = [0];
 
 $(".wr_img_sl img").each(function () {
-	ms_img_src[ms_img_src.length] = $(this).attr("src");
+
+	ms_img_src_min[ms_img_src_min.length] = $(this).attr("src");
+	ms_img_src[ms_img_src.length] = $(this).attr("data-min");
+
 });
 
 
@@ -150,11 +172,10 @@ for (var k = 1; k < ms_img_src.length; k++) {
 	var deg_ret = 360 / ms_img_src.length;
 	deg_ret = deg_ret * k;
 	//$("#cube").append('<div class="fig-'+k+' cn-img" style="background: url('+ms_img_src[k]+') no-repeat; -webkit-transform: rotateX(0deg) rotateY('+deg_ret+'deg) translateX(30px) translateY(0px) translateZ(0px);"><span>'+ k +'</span></div>');
-	$("#cube").append('<div class="fig-' + k + ' cn-img" style="background: url(' + ms_img_src[k] + ') no-repeat; -webkit-transform: rotateX(0deg) rotateY(0deg) translateX(150px) translateY(0px) translateZ(0px);"><a href="' + ms_img_src[k] + '" rel="group" class="fancybox"><span></span></div>');
+	$("#cube").append('<div class="fig-' + k + ' cn-img" style="background: url(' + ms_img_src_min[k] + ') no-repeat; transform: rotateX(0deg) rotateY(0deg) translateX(150px) translateY(0px) translateZ(0px);"><a href="' + ms_img_src[k] + '" rel="group" class="fancybox"><span></span></div>');
 }
 
 
-$(".fancybox").fancybox();
 
 
 //===========================================
@@ -165,13 +186,16 @@ function startSl() {
 
 	$("#cube .cn-img").each(function () {
 		var deg_ret_item = deg_ret * cc;
-		$(this).css("-webkit-transform", "rotateX(0deg) rotateY(" + deg_ret_item + "deg) translateX(150px) translateY(0px) translateZ(0px)");
+		$(this).css("transform", "rotateX(0deg) rotateY(" + deg_ret_item + "deg) translateX(150px) translateY(0px) translateZ(0px)");
 		cc++;
 	});
 
 	$("#cube").addClass("active");
 }
 
+
+
+$(".fancybox").fancybox();
 
 //---------------------------------------------------------
 
